6305645cf984413fb5caa0872b7f47d4ef30c19f
Anastasia Malissovas <anastasiamalissovas@gmail.com>
2019-02-17 17:17:21 -0500

    Javascript in progress

--- public/main.js
+++ public/main.js
@@ -1,7 +1,41 @@
-const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
+const API_URL = 'https://sdg-astro-api.herokuapp.com/api/'
+let launches = []
+const leftBtnClick = () => {
+  console.log('left btn clicked')
+  pageLoad()
+}
+const pageLoad = () => {
+  getLaunchInfo()
+  getHeroImage()
+}
+const getHeroImage = () => {
+  fetch(`${API_URL}Nasa/apod`)
+    .then(response => response.json())
+    .then(response => updateHeroImage(response))
+}
+const updateHeroImage = response => {
+  const heroImage = document.querySelector('.hero-image')
+  heroImage.style.backgroundImage = `url(${response.url})`
+  console.log(response)
+  const imageTitle = document.querySelector('.image-title')
+  imageTitle.innerHTML = response.title
+  const copyright = document.querySelector('.copyright')
+  if (response.copyright === null) {
+    copyright.innerHTML = 'no copyright'
+  } else {
+    copyright.innerHTML = response.copyright
   }
 }
-
-document.addEventListener('DOMContentLoaded', main)
+const getLaunchInfo = () => {
+  fetch(`${API_URL}spacex/launches/upcoming`)
+    .then(response => response.json())
+    .then(response => {
+      launches = response
+    })
+}
+//left button
+document
+  .getElementById('left-arrow-btn')
+  .addEventListener('click', leftBtnClick)
+window.addEventListener('load', pageLoad)
+//right button
