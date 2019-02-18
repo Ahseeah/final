f05d724352eed20f2d89e3f860feb04b4aa10a07
Anastasia Malissovas <anastasiamalissovas@gmail.com>
2019-02-17 21:26:05 -0500

    left and right button working, need to update page.

--- public/main.js
+++ public/main.js
@@ -1,9 +1,10 @@
 const API_URL = 'https://sdg-astro-api.herokuapp.com/api/'
 let launches = []
-const leftBtnClick = () => {
-  console.log('left btn clicked')
-  pageLoad()
-}
+let launchIndex = 0
+let missionName = document.getElementById('mission-name')
+let missionInfo = document.getElementById('mission-info')
+let loc = document.getElementById('loc')
+let counter = document.getElementById('counter')
 const pageLoad = () => {
   getLaunchInfo()
   getHeroImage()
@@ -16,7 +17,6 @@ const getHeroImage = () => {
 const updateHeroImage = response => {
   const heroImage = document.querySelector('.hero-image')
   heroImage.style.backgroundImage = `url(${response.url})`
-  console.log(response)
   const imageTitle = document.querySelector('.image-title')
   imageTitle.innerHTML = response.title
   const copyright = document.querySelector('.copyright')
@@ -29,13 +29,50 @@ const updateHeroImage = response => {
 const getLaunchInfo = () => {
   fetch(`${API_URL}spacex/launches/upcoming`)
     .then(response => response.json())
-    .then(response => {
-      launches = response
-    })
+    .then(response => updateLaunches(response))
 }
+const updateLaunches = response => {
+  launches = response
+}
+//page load, create index at 0
+// display launch at 0 to the document
 //left button
+// when the left button is clicked
+// check if index > 0
+// - 1 from index if it is
+// else set to launch.length -1
+// then we update the document
+const leftBtnClick = () => {
+  if (launchIndex > 0) {
+    launchIndex--
+  } else {
+    launchIndex = launches.length - 1
+  }
+  console.log(launchIndex)
+  updatePage()
+}
+const rightBtnClick = () => {
+  if (launchIndex < launches.length - 1) {
+    launchIndex++
+  } else {
+    launchIndex = 0
+  }
+  console.log(launchIndex)
+  updatePage()
+}
+//right button
+// when the right button is clicked
+// check if index < launch.length - 1
+//  index + 1
+// else set to 0
+// then we update the document
+const updatePage = () => {
+  console.log(launches[launchIndex])
+}
 document
   .getElementById('left-arrow-btn')
   .addEventListener('click', leftBtnClick)
+document
+  .getElementById('right-arrow-btn')
+  .addEventListener('click', rightBtnClick)
 window.addEventListener('load', pageLoad)
-//right button
